a86fe712596ee65e1e68bee347587a1559ecba39
RihanaHayatu <rihanahayatu@gmail.com>
2025-08-19 00:53:20 -0800

    updated department url

--- apps/departments/urls.py
+++ apps/departments/urls.py
@@ -8,7 +8,7 @@ from .views import (
 
 urlpatterns = [
     path('', apply_requirements, name='apply_requirements'),
-    path('departments/', department_submission, name='department_submission'),
-    path('depsuccess/', department_success, name='department_success'),
+    path('submit/', department_submission, name='department_submission'),
+    path('success/', department_success, name='department_success'),
     path('update/<int:pk>/', DepartmentUpdate.as_view(), name='department_update'),
 ]
